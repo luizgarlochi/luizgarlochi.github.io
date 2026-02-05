3dd9b86f41c4c8a89ef2c3e663e595cb52b1a8ee
Luiz <luiz.arlochi@gmail.com>
2026-02-05 17:10:52 -0300

    [FIX] - removendo nome do navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,10 +52,6 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold flex">
-            Luiz Guilherme&nbsp;
-            <span className="sm:block hidden">| JS-Python</span>
-          </p>
         </Link>
 
         {/* Desktop Menu */}
